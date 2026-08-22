5ebdde7d6e6f561cc4a81aefed8e01962f1cab49
CSE370 Student <student@bracu.ac.bd>
2026-08-22 19:03:00 +0600

    Update system skeleton with core deliverables and clean connection config

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ def get_db_connection():
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="",       # Default XAMPP password is empty
+            password="",      
             database="hospital_db"
         )
         return connection
